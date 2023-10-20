ecf726ac50bd9c195ae9d806dc94cf3bbb11f86a
MaxPredathor <max.gilli98@gmail.com>
2023-10-20 17:27:42 +0200

    ticket

--- js/script.js
+++ js/script.js
@@ -1,43 +1,68 @@
 (function(){
 
     const btn = document.querySelector('button');
-    const input = document.querySelector('input').value;
+    const input = document.querySelector('input');
     const alert = document.getElementById('alert');
+    const btnInput = document.getElementById('button2');
+    let randomArray = [];
+    let inputArray = [];
 
 
     btn.addEventListener('click', function(){
 
-        removeDisplayNone();
-        myCicle();
+        displayNoneReset();
+        randomNumbersArray();
         setTimeout(addDisplayNone, 3000);
     });
 
+    btnInput.addEventListener('click', function(){
+        let inputValue = input.value;
+        inputArray.push(inputValue);
+        console.log(inputArray);
 
+        let i = 0;
+        while(i < 5){
+            inputArray[0].split(" ");
+            console.log(inputArray);
+            inputArray.map(Number);
+            console.log(inputArray);
 
-
+        i++
+        }           
+    });
 
     function getRndInteger(min, max) {
         return Math.floor(Math.random() * (max - min + 1) ) + min;
     }
 
-    function myCicle(){
+    function randomNumbersArray(){
         let num;
-        let inputArray = [];
         let i = 0;
         while(i < 5){
             num = getRndInteger(1, 100);
-            inputArray.push(num);
+            randomArray.push(num);
         i++
         }
-        alert.innerText = inputArray;
-        console.log(inputArray);
+        alert.style.color = 'white';
+        alert.style.fontWeight = 'bold';
+        alert.style.fontSize = '25px';
+        alert.innerText = randomArray;
+        console.log(randomArray);
     }
+    
     function addDisplayNone(){
         alert.classList.add('d-none');
+        input.classList.remove('d-none');
+        btnInput.classList.remove('d-none');
+        btn.classList.add('d-none');
+
     }
 
-    function removeDisplayNone(){
+    function displayNoneReset(){
         alert.classList.remove('d-none');
+        input.classList.add('d-none');
+        btnInput.classList.add('d-none');
+        btn.classList.remove('d-none');
     }
 
 }) ();
